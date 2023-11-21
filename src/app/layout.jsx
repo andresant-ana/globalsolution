d282d0f0345f1fe27ef23e74aa6db53152abb182
andresant-ana <rm551575@fiap.com.br>
2023-11-21 18:35:35 -0300

    Criado cabeçalho e rodapé utilizando components.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,9 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import Cabecalho from '@/components/Cabecalho/Cabecalho'
+import Rodape from '@/components/Rodape/Rodape'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Cabecalho/>
+        {children}
+        <Rodape/>
+      </body>
     </html>
   )
 }
