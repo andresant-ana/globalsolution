44f2d65b1d19c02bc0607a88988184a368697acf
andresant-ana <rm551575@fiap.com.br>
2023-11-21 21:09:01 -0300

    Estilização de cabeçalho/rodapé + estilização página 404 + estilização homepage + add imagem notredame e perfil-icon
    Co-authored-by: Gabriel Eringer <GEdO23@users.noreply.github.com>

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
-    <div className="erro404">
-        <h1>404</h1>
-        <p>Página não encontrada!</p>
-    </div> 
+    <div className="font-extrabold h-full w-full fixed">
+        <h1 className="text-6xl text-red-500 text-center mb-7 mt-7">404</h1>
+        <p className="text-red-500 text-center text-3xl mt-7">Página não encontrada!</p>
+    </div>
   )
 }
--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,47 +1,71 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Cabecalho() {
-    //No momento de recuperar o objeto da sessão, é necessário converter o JSON para objeto novamente.
-    const usuario = JSON.parse(sessionStorage.getItem("obj-user"));
-    const [userLogado] = useState(usuario);
-  
-    const handleLogout = () => {
-      sessionStorage.removeItem("obj-user");
-      sessionStorage.removeItem("token-user");
-      window.location.href = "/";
-    }
-  
-    if(sessionStorage.getItem("token-user") != null){
-    return (
-      <header className='cabecalho'>
-        <div>
-          <p>{usuario != null ? `Olá ${usuario.email}`:""}</p>
-        </div>
-      <nav>
-        <ul>
-          <li><Link href="/">HOME</Link></li>
-          <li><Link href="/ia">IA</Link></li>
-          <li><Link href="/sobre">SOBRE</Link></li>
-          <li><Link href="/contato">CONTATO</Link></li>
-          <li><Link href="/" onClick={handleLogout}>LOGOUT</Link></li>
+  //No momento de recuperar o objeto da sessão, é necessário converter o JSON para objeto novamente.
+  const usuario = JSON.parse(sessionStorage.getItem("obj-user"));
+  const [userLogado] = useState(usuario);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("obj-user");
+    sessionStorage.removeItem("token-user");
+    window.location.href = "/";
+  };
+
+  const usuarioNaoLogado = sessionStorage.getItem("token-user") != null;
+
+  return (
+    <header className="cabecalho">
+      <div className="cabecalho__logo">
+        <Image
+        src="/notredame_logo.png"
+        alt=""
+        width="116"
+        height="32"/>
+      </div>
+      <nav className="cabecalho__navegacao">
+        <ul className="menu">
+          <li className="menu__item">
+            <Link href="/" className="menu__item__link">HOME</Link>
+          </li>
+          <li className="menu__item">
+            <Link href="/sobre" className="menu__item__link">SOBRE</Link>
+          </li>
+          {
+            usuarioNaoLogado ? (
+              <>
+                <li className="menu__item">
+                  <Link href="/ia" className="menu__item__link">IA</Link>
+                </li>
+                
+                <li className="menu__item">
+                  <Link href="/" onClick={handleLogout} className="menu__item__link">LOGOUT</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="menu__item">
+                  <Link href="/cadastro" className="menu__item__link">CADASTRE-SE</Link>
+                </li>
+
+                <li className="menu__item">
+                  <Link href="/login" className="menu__item__link">LOGIN</Link>
+                </li>
+              </>
+            )
+          }
         </ul>
       </nav>
+      <div className="cabecalho__usuario">
+        <Image className="cabecalho__usuario__foto-de-perfil" 
+        src="/perfil_icone.svg" 
+        alt="" 
+        width="50" 
+        height="50" />
+        <p className="cabecalho__usuario__email">{usuario != null ? `${usuario.email}` : "undefined"}</p>
+      </div>
     </header>
-    )
-    }else{
-      return (
-        <header className='cabecalho'>
-        <nav>
-          <ul>
-            <li><Link href="/">HOME</Link></li>
-            <li><Link href="/sobre">SOBRE</Link></li>
-            <li><Link href="/login">LOGIN</Link></li>
-            <li><Link href="/cadastro">CADASTRE-SE</Link></li>
-          </ul>
-        </nav>
-      </header>
-      )
-    }
-  }
+  )
+}
