ce7db858fbbf60f3641db12507c4e9d246ba0ac8
GEdO23 <rm99632@fiap.com.br>
2023-11-24 03:47:59 -0300

    Teste na API local, de POST(cadastro de usuário)

---
+++ src/app/api/base/base-cad/route.jsx
@@ -0,0 +1,28 @@
+import {promises as fs} from "fs";
+import { NextResponse } from "next/server";
+
+export async function POST(request, response) {
+    try {
+        const userRequest = await request.json();
+
+        // Obtendo arquivo db.json em forma de string
+        const file = await fs.readFile(process.cwd() + '/src/app/api/base/db.json', 'utf-8');
+
+        // Transformando arquivo em um objeto e armazenando na variavel lista
+        const lista = await JSON.parse(file);
+
+        // Obtendo o maior id na lista e alterando id recebido pelo usuário
+        const newId = lista.usuarios[lista.usuarios.length - 1].id + 1;
+        userRequest.id = newId;
+
+        // Adcionando userRequest a lista e reescrevendo arquivo com essa nova lista
+        lista.usuarios.push(userRequest);
+        fs.writeFile(process.cwd() + '/src/app/api/base/db.json', JSON.stringify(lista));
+
+        return NextResponse.json(userRequest);
+    } catch(error) {
+        console.log(error);
+    }
+
+    return NextResponse.json({"status": false});
+}
