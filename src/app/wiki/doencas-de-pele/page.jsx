f65725cd2d96c49c35966979d1850f88eaf484fe
GEdO23 <rm99632@fiap.com.br>
2023-11-24 23:39:59 -0300

    Correção de bug onde todas as doenças abrem ou todas fecham.

--- src/app/wiki/doencas-de-pele/page.jsx
+++ src/app/wiki/doencas-de-pele/page.jsx
@@ -7,13 +7,13 @@ export default function DoencasDePele() {
 
     const [doencasDePele, setDoencasDePele] = useState([]);
 
-    useEffect(()=> {
-        const fetchData = async ()=> {
+    useEffect(() => {
+        const fetchData = async () => {
             try {
                 const response = await fetch('http://localhost:3000/api/base/base-doencas/0');
                 const data = await response.json();
                 setDoencasDePele(data);
-                
+
             } catch (error) {
                 console.log(error);
             }
@@ -22,105 +22,43 @@ export default function DoencasDePele() {
         fetchData();
     }, []);
 
-    const [containerOpen, setContainerOpen] = useState(false);
+    const [doencasAbertas, setDoencasAbertas] = useState({});
+
+    const toggleContainer = (id) => {
+        setDoencasAbertas(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
 
     return (
         <main className='w-full h-full p-[20px] pb-[50px] flex flex-col items-center gap-[25px] bg-[#EDF7F6]'>
             <article className='w-full max-w-[800px] flex flex-col items-center gap-[25px]'>
                 {
                     doencasDePele.map((doenca) => (
-                        <>
-                            <section key={doenca.id} className='w-full flex flex-col gap-[10px]'>
-                                <div onClick={()=> setContainerOpen(!containerOpen)} className='w-full px-[25px] py-[13px] flex justify-between items-center bg-white rounded-[10px] shadow-sm cursor-pointer'>
-                                    <h2 className=' font-bold text-[1.2rem] text-[#080708]'>{doenca.nome}</h2>
-                                    <IoMdArrowDropdown className='text-[1.2rem]' />
-                                </div>
+                        <section key={doenca.id} className='w-full flex flex-col gap-[10px]'>
+                            <div onClick={() => toggleContainer(doenca.id)} className='w-full px-[25px] py-[13px] flex justify-between items-center bg-white rounded-[10px] shadow-sm cursor-pointer'>
+                                <h2 className=' font-bold text-[1.2rem] text-[#080708]'>{doenca.nome}</h2>
+                                {doencasAbertas[doenca.id] ? <IoMdArrowDropup className='text-[1.2rem]' /> : <IoMdArrowDropdown className='text-[1.2rem]' />}
+                            </div>
 
-                                {
-                                    containerOpen ?
-                                    <DropdownContainer 
-                                    nome={doenca.nome} 
-                                    descricao={doenca.descricao} 
-                                    sintomas={doenca.sintomas} 
-                                    imagemSintomas={doenca.imagemSintomas} 
-                                    causas={doenca.causas}
-                                    imagemCausas={doenca.imagemCausas}
-                                    tratamentos={doenca.tratamentos}
+                            {
+                                doencasAbertas[doenca.id] ?
+                                    <DropdownContainer
+                                        nome={doenca.nome}
+                                        descricao={doenca.descricao}
+                                        sintomas={doenca.sintomas}
+                                        imagemSintomas={doenca.imagemSintomas}
+                                        causas={doenca.causas}
+                                        imagemCausas={doenca.imagemCausas}
+                                        tratamentos={doenca.tratamentos}
                                     /> :
-                                    <></>
-                                
-                                }
-
-                                {/* <article className='w-full rounded-[10px] flex flex-col gap-[25px] p-[25px] pb-[50px] bg-white shadow-sm'>
-                                    <section className='w-full flex gap-[5px]'>
-                                        <p><strong className='font-semibold text-base text-[#080708]'>Nome:</strong></p>
-                                        <p className='font-normal text-base text-[#080708]'>{doenca.nome}</p>
-                                    </section>
-
-                                    <section className='w-full flex flex-col gap-[5px]'>
-                                        <p><strong className='font-semibold text-base text-[#080708]'>Descrição:</strong></p>
-                                        <p className='font-normal text-base text-[#080708]'>{doenca.descricao}</p>
-                                    </section>
-
-                                    <section className='w-full flex flex-wrap gap-x-[20px] gap-y-[25px]'>
-                                        <div className='w-full flex flex-col gap-[5px]'>
-                                            <p><strong className='font-semibold text-base text-[#080708]'>Sintomas:</strong></p>
-                                            <ul className='pl-[25px]'>
-                                                {
-                                                    doenca.sintomas.map((sintoma)=> (
-                                                        <>
-                                                        <li className='break-words font-normal text-base text-[#080708] list-disc'>{sintoma}</li>
-                                                        </>
-                                                    ))
-                                                }
-                                            </ul>
-                                        </div>
-                                        <div className='w-full'>
-                                            <Image src={doenca.imagemSintomas} alt="" width="365" height="225"/>
-
-                                        </div>
-                                    </section>
-
-                                    <section className='w-full flex flex-wrap gap-x-[20px] gap-y-[25px]'>
-                                        <div className='w-full flex flex-col gap-[5px]'>
-                                            <p><strong className='font-semibold text-base text-[#080708]'>Causas:</strong></p>
-                                            <ul className='pl-[25px]'>
-                                                {
-                                                    doenca.causas.map((causa)=> (
-                                                        <>
-                                                        <li className='font-normal text-base text-[#080708] list-disc'>{causa}</li>
-                                                        </>
-                                                    ))
-                                                }
-                                            </ul>
-                                        </div>
-                                        <div className='w-full'>
-                                            <Image src={doenca.imagemCausas} alt="" width="365" height="225"/>
-
-                                        </div>
-                                    </section>
-
-                                    <section className='w-full flex flex-col gap-[5px]'>
-                                        <p><strong className='font-semibold text-base text-[#080708]'>Tratamentos:</strong></p>
-                                        <ul className='pl-[25px]'>
-                                            {
-                                                doenca.tratamentos.map((tratamento)=> (
-                                                    <>
-                                                    <li className='font-normal text-base text-[#080708] list-disc'>{tratamento}</li>
-                                                    </>
-                                                ))
-                                            }
-                                        </ul>
-                                    </section>
-                                </article> */}
-
-                            </section>
-                        </>
+                                    null
+                            }
+                        </section>
                     ))
                 }
-
             </article>
-
         </main>
-    )
+    );
 }
